c41b2138a53aba595a3b00e084fcd54dc1125b63
sadielise <sadiet@rams.colostate.edu>
2015-12-01 17:43:54 -0700

    Initial commit

---
+++ app/src/main/java/groupl/cs314/quizapp/mainscreen.java
@@ -0,0 +1,13 @@
+package groupl.cs314.quizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mainscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainscreen);
+    }
+}
