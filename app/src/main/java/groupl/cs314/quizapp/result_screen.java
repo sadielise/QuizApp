69010ee905e0cd40cd9313576623a1db3c5d9307
gfontani <gabriella@fontani.net>
2015-12-03 20:53:01 -0700

    Corrected the result page activity!!

---
+++ app/src/main/java/groupl/cs314/quizapp/result_screen.java
@@ -0,0 +1,14 @@
+package groupl.cs314.quizapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class result_screen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_screen);
+    }
+
+}
