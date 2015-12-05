853b5d1f2e1eb701e52c78d0d44c8a9c0e1ba037
sadielise <sadiet@rams.colostate.edu>
2015-12-04 21:44:59 -0700

    adding onclick methods for buttons, re-doing the gui

--- app/src/main/java/groupl/cs314/quizapp/QuizScreen.java
+++ app/src/main/java/groupl/cs314/quizapp/QuizScreen.java
@@ -2,6 +2,8 @@ package groupl.cs314.quizapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Toast;
 
 public class QuizScreen extends Activity {
 
@@ -9,6 +11,22 @@ public class QuizScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_screen);
+    }
+
+    public void onClickBack(View view){
+        //Toast.makeText(QuizScreen.this, "Previous question", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickAnswer(View view){
+        //Toast.makeText(QuizScreen.this, "Answered the question", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickNext(View view){
+        //Toast.makeText(QuizScreen.this, "Next question", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickFinish(View view){
+        //sToast.makeText(QuizScreen.this, "Finish quiz", Toast.LENGTH_SHORT).show();
     }
 
 }
--- app/src/main/java/groupl/cs314/quizapp/mainscreen.java
+++ app/src/main/java/groupl/cs314/quizapp/mainscreen.java
@@ -12,19 +12,11 @@ import com.google.android.gms.common.api.GoogleApiClient;
 
 public class mainscreen extends AppCompatActivity {
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainscreen);
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
     }
 
     // Called when used clicks the Start Quiz button
@@ -33,44 +25,5 @@ public class mainscreen extends AppCompatActivity {
         startActivity(intent);
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction;
-        viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Start Quiz Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse(null),
-                /* TODO: Make sure this auto-generated app deep link URI is correct. */
-                Uri.parse("android-app://groupl.cs314.quizapp/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Start Quiz Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://groupl.cs314.quizapp/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }
 }
