5e23a8fc3351a12268698dab3527e954e3fee94d
gfontani <gabriella@fontani.net>
2015-12-05 11:01:16 -0700

    Merge branch 'master' of github.com:sadielise/QuizApp Added all buttons for result screen
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/groupl/cs314/quizapp/result_screen.java
+++ app/src/main/java/groupl/cs314/quizapp/result_screen.java
@@ -1,5 +1,6 @@
 package groupl.cs314.quizapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Gravity;
@@ -14,13 +15,26 @@ public class result_screen extends Activity {
         setContentView(R.layout.activity_result_screen);
     }
 
+    public void ExitApp()
+    {
+
+        this.finish();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
     public void onClickExit(View view){
         // go back to previous question unless you're on the first question
             Toast toast = Toast.makeText(result_screen.this, "Thanks for Playing!", Toast.LENGTH_SHORT);
-            toast.setGravity(Gravity.CENTER_VERTICAL| Gravity.CENTER_HORIZONTAL, 0, 0);
+            toast.setGravity(Gravity.CENTER_VERTICAL| Gravity.CENTER_HORIZONTAL, 0, 300);
             toast.show();
+            ExitApp();
+    }
 
-            finish();
-            System.exit(0);
+    public void onClickPlayAgain(View view) {
+        Intent intent = new Intent(this, start_screen.class);
+        startActivity(intent);
     }
 }
