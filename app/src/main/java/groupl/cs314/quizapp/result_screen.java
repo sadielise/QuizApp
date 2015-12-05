4410efb5f36e180f3dc3d86ecebcd442411f4806
gfontani <gabriella@fontani.net>
2015-12-05 10:29:41 -0700

    Merge branch 'master' of github.com:sadielise/QuizApp Adding buttons to result screen still
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/groupl/cs314/quizapp/result_screen.java
+++ app/src/main/java/groupl/cs314/quizapp/result_screen.java
@@ -2,6 +2,9 @@ package groupl.cs314.quizapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
 
 public class result_screen extends Activity {
 
@@ -11,4 +14,13 @@ public class result_screen extends Activity {
         setContentView(R.layout.activity_result_screen);
     }
 
+    public void onClickExit(View view){
+        // go back to previous question unless you're on the first question
+            Toast toast = Toast.makeText(result_screen.this, "Thanks for Playing!", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER_VERTICAL| Gravity.CENTER_HORIZONTAL, 0, 0);
+            toast.show();
+
+            finish();
+            System.exit(0);
+    }
 }
